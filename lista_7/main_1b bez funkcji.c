1825504fb0c9b80d332947bb623bf90a3c256c35
Marcin521 <marcinkondo134@gmail.com>
2021-05-17 15:18:08 +0200

    add loading matrix, add search min

--- lista_7/main_1b bez funkcji.c
+++ lista_7/main_1b bez funkcji.c
@@ -1,8 +1,57 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+/*Zad. 1. 
+W tablicy posiadaj¹cej n wierszy i m kolumn przechowywane s¹ dane o energii elektrycznej pobranej w przeci¹gu jednego roku. 
+Poszczególne wiersze odpowiadaj¹ poszczególnym odbiorcom, kolumny – miesi¹com. Nale¿y:
 
-int main(int argc, char *argv[]) {
+b.   dla ka¿dego odbiorcy okreœliæ minimaln¹ energiê pobran¹ w ci¹gu jednego miesi¹ca,
+*/
+
+
+int main(){
+
+	int a[10][10], min, b, i, j, n, m;
+	
+	min=1000;
+	
+	printf("podaj l wierszy ");
+	scanf("%d", &n);
+	
+	printf("\n podaj l kolumn ");
+	scanf("%d", &m);
+	
+	
+	
+	//wype³nianie macierzy
+		for(i=0; i<m; i++){
+		
+		for(j=0; j<n; j++){
+		
+			printf("\n podaj a[%d, %d] ", i ,j);
+			scanf("%d", &a[i][j]);
+			
+			if(min>a[i][j]){
+			
+				min=a[i][j];
+					
+			}									
+				
+				printf("min %d", a[i][j]);
+					
+			}						
+		}
+			
+	
+	//drukowanie macierzy
+		for(i=0; i<m;i++)
+		{
+			for(j=0; j<n; j++){
+				printf(" %d ", a[i][j]);
+			}
+			printf(" \n");
+			
+		}
+	
 	return 0;
 }
