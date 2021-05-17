889d4561d2d7cefe36fbe99bff0ff50cb0ddba40
Marcin521 <marcinkondo134@gmail.com>
2021-05-17 19:03:33 +0200

    add finish project

--- lista_7/main_zad_3.c
+++ lista_7/main_zad_3.c
@@ -9,6 +9,66 @@ Wczytaæ elementy dwóch macierzy kwadratowych o wymiarach n × n
 Obliczyæ elementy macierzy, bêd¹cej sum¹ wczytanych macierzy.
 
 */
-int main(){
-	return 0;
+
+int main() {
+	
+	int a[10][10], b[10][10], c[10][10], i, j , n, m;
+	
+	printf("\n podaj n wierszy ");
+	scanf("%d", &n);
+	
+	if(n>11) return 0;
+	
+	
+	//wype³nianie macierzy
+	for(i=0; i<n; i++){
+		
+		for(j=0; j<n; j++){
+		
+			printf("\n podaj a[%d, %d] ", i ,j);
+			scanf("%d", &a[i][j]);
+	
+			printf("\n podaj b[%d, %d] ", i ,j);
+			scanf("%d", &b[i][j]);
+			
+			c[i][j]=a[i][j]+b[i][j];						
+		}
+	}
+	
+	printf("\n macierz ab \n");
+	
+	for(i=0; i<n; i++)
+		{
+			for(j=0; j<n; j++){
+				printf(" %d ", a[i][j]);
+			}
+			printf(" \n");
+			
+		}
+	
+	printf("\n macierz b \n");
+		
+	for(i=0; i<n; i++)
+		{
+			for(j=0; j<n; j++){
+				printf(" %d ", b[i][j]);
+			}
+			printf(" \n");
+			
+		}
+	
+	printf("\n wynik sumowania \n");
+	
+	//drukowanie wyniku
+		for(i=0; i<n; i++)
+		{
+			for(j=0; j<n; j++){
+				printf(" %d ", c[i][j]);
+			}
+			printf(" \n");
+			
+		}
+		
+		
+		
 }
