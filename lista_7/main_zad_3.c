0856296d1d07355dd4e6a9acb4e5cf13c85c52dc
Marcin521 <marcinkondo134@gmail.com>
2021-05-17 18:40:17 +0200

    add new project

---
+++ lista_7/main_zad_3.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+Zad. 3. 
+
+Wczytaæ elementy dwóch macierzy kwadratowych o wymiarach n × n 
+(n – wczytywane do programu, n ? 10).
+Obliczyæ elementy macierzy, bêd¹cej sum¹ wczytanych macierzy.
+
+*/
+int main(){
+	return 0;
+}
