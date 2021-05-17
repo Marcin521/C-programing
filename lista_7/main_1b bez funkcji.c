df32d254ed90f0aaefbdbbfc1a0e354bcc28e7e3
Marcin521 <marcinkondo134@gmail.com>
2021-05-17 18:17:10 +0200

    fix min

--- lista_7/main_1b bez funkcji.c
+++ lista_7/main_1b bez funkcji.c
@@ -11,7 +11,7 @@ b.   dla ka¿dego odbiorcy okreœliæ minimaln¹ energiê pobran¹ w ci¹gu jednego mie
 
 int main(){
 
-	int a[10][10], min, b, i, j, n, m;
+	int a[10][10], b[2][2], min, i, j, n, m;
 	
 	min=1000;
 	
@@ -31,17 +31,23 @@ int main(){
 			printf("\n podaj a[%d, %d] ", i ,j);
 			scanf("%d", &a[i][j]);
 			
+			
 			if(min>a[i][j]){
 			
-				min=a[i][j];
-					
+				min=a[i][j];		
 			}									
-				
-				printf("min %d", a[i][j]);
-					
+			
+									
 			}						
 		}
-			
+				printf("\n =======");
+				printf("\n = min %d", min);
+				printf("\n = i %d", i);
+				printf("\n = j %d", j);
+				printf("\n =======");
+					
+	
+	printf("\n");
 	
 	//drukowanie macierzy
 		for(i=0; i<m;i++)
