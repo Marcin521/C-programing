a93955ca65a6960f2ad617697346959087b336b9
Marcin521 <marcinkondo134@gmail.com>
2021-05-21 12:26:15 +0200

    add funcion

--- lista_8/main_8.c
+++ lista_8/main_8.c
@@ -1,8 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+/*
+Zad. 8.
+ 
+Napisz funkcjê, która znajduje sumê elementów 
+oraz sumê wartoœci bezwzglêdnych elementów tablicy.
+*/
 
-int main(int argc, char *argv[]) {
+void suma(int tab[], int *sum){
+	
+	int i;
+
+	for (i=0; i<5; i++){
+	
+		if(tab[i] <0) tab[i]*=(-1);
+		
+		*sum += tab[i];
+
+	}
+	
+	printf("suma %d", *sum);
+}
+
+	int tab1[5] ={8, -32, 3 ,5,6};
+	int sum;
+
+int main() {
+	
+	suma(tab1, &sum);
 	return 0;
 }
