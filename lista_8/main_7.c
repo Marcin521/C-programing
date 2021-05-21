c763bf930140f6559e736c52ba5480f20c41d476
Marcin521 <marcinkondo134@gmail.com>
2021-05-21 13:56:55 +0200

    add new project

--- lista_8/main_7.c
+++ lista_8/main_7.c
@@ -1,8 +1,48 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+/* 
+Zad. 7.
 
-int main(int argc, char *argv[]) {
+ Napisz program, w którym funkcja 
+ bêdzie zamienia³a wartoœci zmiennych
+*/
+void exchange(int A[], int B[]){
+	
+	int i;
+	int rezA[5];
+	
+	for (i=0; i<5; i++){
+	
+		A[i] = rezA[i];
+		A[i] = B[i];
+		
+		B[i] = rezA[i]; 
+		
+	printf(" tab %d", A[i]);
+	
+
+	}
+
+	printf("\n");
+	
+	for (i=0; i<5; i++){
+	
+	B[i] = rezA[i]; 
+
+	printf(" ntab %d", B[i]);
+	
+	}
+	
+	
+}
+
+int tab1[5] ={1, 2, 3, 4, 5};
+int tab2[5] ={5, 4, 3, 2, 1};
+
+int main() {
+
+	exchange(&tab1, &tab2);
+	
 	return 0;
 }
