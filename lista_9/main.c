f247ff7b1143e521a5b3bd5ae69c677a3445080b
Marcin521 <marcinkondo134@gmail.com>
2021-05-28 11:59:29 +0200

    add struct in program

--- lista_9/main.c
+++ lista_9/main.c
@@ -1,8 +1,49 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+/*Zad. 1. Wype³nij z klawiatury, a nastêpnie wyœwietl na ekranie tablicê struktur zawieraj¹cych informacje o studentach. Informacjami tymi s¹:
 
-int main(int argc, char *argv[]) {
+–  nazwisko i imiê studenta,
+–  rok studiów,
+–  nr grupy,
+
+
+*/
+
+struct dane{
+	char imie[10];
+	char nazwisko[10];
+	int grupa[10];
+};
+
+int main() {
+	
+	struct dane tab[10];
+	
+	int i, n;
+	
+	for(i=0; i<n; i++){
+		printf("imie ");
+		scanf("%s", &tab[i].imie);
+		
+		printf("nzawisko ");
+		scanf("%s", &tab[i].nazwisko);
+		
+		printf("grupa ");
+		scanf("%s", &tab[i].grupa);
+		
+	}
+	
+	//drukuj
+	
+	printf(" Imie Nazwisko grupa \n");
+	
+	for(i=0; i<n; i++){
+		
+		printf("%10s %10s %10s ", 
+		tab[i].imie, tab[i].nazwisko, tab[i].grupa);		
+		
+	}
+	
 	return 0;
 }
